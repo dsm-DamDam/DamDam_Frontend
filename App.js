cecc86693156fc8593881567770accc1fda781d6
Kangyongsoo <aodtn2006@naver.com>
2023-06-13 19:07:35 +0900

    📝 :: (#01) theme/font 설정

--- App.js
+++ App.js
@@ -1,20 +1,34 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { View } from "react-native";
+import { useFonts } from "expo-font";
+import { setCustomText } from "react-native-global-props";
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    "Roboto-Black": require("./src/assets/fonts/Roboto/Roboto-Black.ttf"),
+    "Roboto-Bold": require("./src/assets/fonts/Roboto/Roboto-Bold.ttf"),
+    "Roboto-Medium": require("./src/assets/fonts/Roboto/Roboto-Medium.ttf"),
+    "Roboto-Regular": require("./src/assets/fonts/Roboto/Roboto-Regular.ttf"),
+    "Roboto-Light": require("./src/assets/fonts/Roboto/Roboto-Light.ttf"),
+    "NotoSansKR-Black": require("./src/assets/fonts/NotoSansKR/NotoSansKR-Black.otf"),
+    "NotoSansKR-Bold": require("./src/assets/fonts/NotoSansKR/NotoSansKR-Bold.otf"),
+    "NotoSansKR-Medium": require("./src/assets/fonts/NotoSansKR/NotoSansKR-Medium.otf"),
+    "NotoSansKR-Regular": require("./src/assets/fonts/NotoSansKR/NotoSansKR-Regular.otf"),
+    "NotoSansKR-Light": require("./src/assets/fonts/NotoSansKR/NotoSansKR-Light.otf"),
+  });
+  if (!fontsLoaded) {
+    return <StatusBar />;
+  }
+  const customTextProps = {
+    style: {
+      fontFamily: "NotoSansKR-Medium",
+    },
+  };
+  setCustomText(customTextProps);
+
   return (
-    <View style={styles.container}>
-      <Text>DamDam</Text>
+    <View>
       <StatusBar style="auto" />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
