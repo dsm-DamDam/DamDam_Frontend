cf129dc2cb08e5ea2172e92905b5f8f98325a6bb
Kangyongsoo <aodtn2006@naver.com>
2023-06-21 11:44:30 +0900

    ✨:: (#01)useInput

---
+++ src/hooks/useInput.js
@@ -0,0 +1,16 @@
+import { useState } from "react";
+
+// initialValue = 초기값, regex = 정규식
+export const useInput = (initialValue, regex) => {
+  const [value, setValue] = useState(initialValue);
+  const onChangeText = (event) => {
+    let willUpdate = true;
+    if (typeof regex === "string") {
+      willUpdate = regex.test(value);
+    }
+    if (willUpdate) {
+      setValue(event);
+    }
+  };
+  return [value, onChangeText];
+};
