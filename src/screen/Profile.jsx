e65e518da838dfa9b4cc2cd8b9232f490ab48ccd
MaJaeYoung <dabeta1234@gmail.com>
2023-07-25 20:48:35 +0900

    ✨ :: (#05)menuComponent추가
    icon 추가

--- src/screen/Profile.jsx
+++ src/screen/Profile.jsx
@@ -1,10 +1,17 @@
 import { Text } from "react-native";
 import styled from "styled-components";
+import { theme } from "../style/theme";
+import _User_info from "../assets/icons/_user_info";
 
-function Profile({ navigation }) {
+function Profile() {
   return (
     <Container>
       <Text>Profile</Text>
+      <Userbox>
+        <UserCircle>
+          <_User_info />
+        </UserCircle>
+      </Userbox>
     </Container>
   );
 }
@@ -13,6 +20,24 @@ const Container = styled.View`
   flex: 1;
   justify-content: center;
   align-items: center;
+`;
+
+const Userbox = styled.TouchableOpacity`
+  align-items: center;
+  justify-content: center;
+  height: 85px;
+  width: 85%;
+  border-radius: 10px;
+  background-color: ${theme.color.gray_100};
+`;
+
+const UserCircle = styled.View`
+  width: 40px;
+  height: 40px;
+  border-radius: 50px;
+  border: 1px solid ${theme.color.gray_300};
+  align-items: center;
+  justify-content: center;
 `;
 
 export default Profile;
